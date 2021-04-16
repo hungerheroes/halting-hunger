750c116c37613197e930ef48e6dd184992128cec
Athma-Vaishali <athmavaishali@gmail.com>
2021-04-16 18:03:40 -0500

    resp login

--- app/src/main/java/com/example/haltinghunger/UserClass.java
+++ app/src/main/java/com/example/haltinghunger/UserClass.java
@@ -1,4 +1,18 @@
 package com.example.haltinghunger;
 
-public class UserClass {
+import com.parse.ParseClassName;
+import com.parse.ParseFile;
+import com.parse.ParseObject;
+import com.parse.ParseUser;
+
+import java.util.Date;
+
+@ParseClassName("User")
+public class UserClass extends ParseObject{
+    public static final String KEY_TYPE="type";
+
+    public String getType(){
+        return getString(KEY_TYPE);
+    }
 }
+
