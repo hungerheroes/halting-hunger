637602937a5074d6069d018463f4aa265545ad71
bhavanamb <mbbhavana9@gmail.com>
2021-04-16 11:59:51 -0500

    added SingUp screen

--- app/src/main/java/com/example/haltinghunger/MainActivity.java
+++ app/src/main/java/com/example/haltinghunger/MainActivity.java
@@ -2,13 +2,29 @@ package com.example.haltinghunger;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button signUpBtn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        signUpBtn = (Button) findViewById(R.id.btnSignUp);
+        signUpBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openRegisterActivity();
+            }
+        });
     }
+
+    private void openRegisterActivity() {
+        Intent intent = new Intent(this,SignUp.class);
+        startActivity(intent);
+    }
+
 }
