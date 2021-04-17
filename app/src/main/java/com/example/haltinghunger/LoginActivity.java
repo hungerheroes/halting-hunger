cc635e4d6d7f8e64015157d48ab6002048225ac2
Athma-Vaishali <athmavaishali@gmail.com>
2021-04-16 21:52:56 -0500

    create_post xml

--- app/src/main/java/com/example/haltinghunger/LoginActivity.java
+++ app/src/main/java/com/example/haltinghunger/LoginActivity.java
@@ -14,6 +14,7 @@ import com.parse.LogInCallback;
 import com.parse.ParseException;
 import com.parse.ParseUser;
 
+
 public class LoginActivity extends AppCompatActivity {
     public static final String TAG="LoginActivity";
     private EditText etUsername;
@@ -59,7 +60,7 @@ public class LoginActivity extends AppCompatActivity {
                     return;
                 }
                 gotoHomeOfUser();
-//                ParseUser.getCurrentUser().get
+//                ParseUser.getCurrentUser().UserClass.gettype();
                 Toast.makeText(LoginActivity.this,"Success!", Toast.LENGTH_SHORT).show();
             }
         });
