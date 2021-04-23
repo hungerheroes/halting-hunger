74bc6e4947e91d2e74de5c8a61fb2b6f411f2061
Athma-Vaishali <athmavaishali@gmail.com>
2021-04-23 18:41:16 -0500

    logout in volunteer page

--- app/src/main/java/com/example/haltinghunger/HomeBeneficiary.java
+++ app/src/main/java/com/example/haltinghunger/HomeBeneficiary.java
@@ -1,9 +1,15 @@
 package com.example.haltinghunger;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.RecyclerView;
+
+import com.parse.ParseUser;
 
 import java.util.List;
 
@@ -20,5 +26,24 @@ public class HomeBeneficiary extends AppCompatActivity {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.beneficiary_home);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu,menu);   
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if(item.getItemId()==R.id.Logout){
+            ParseUser.logOut();
+            ParseUser currentUser = ParseUser.getCurrentUser();
+            Intent i =new Intent(this,LoginActivity.class);
+            startActivity(i);
+            finish();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
